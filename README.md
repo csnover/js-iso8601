cb9de67b3eb486a83a1824427e31896ef7fc3b79
Colin Snover <github.com@zetafleet.com>
2011-11-12 21:47:58 +0000

    Non-conforming edition with extra features.

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
-Universal JavaScript Date.parse for ISO 8601
-============================================
+Universal JavaScript Date.parse for ISO 8601, non-conformant edition
+====================================================================
 
 ECMAScript revision 5 adds native support for simplified [ISO 8601](http://en.wikipedia.org/wiki/ISO_8601) dates in the
 `Date.parse` method, but some browsers currently on the market (Safari 5-, IE 8-, Firefox 3.6-) do not support it. This
-is a simple shim for Date.parse that adds support for parsing ES5 simplified ISO 8601 strings to all browsers.
+is a simple shim for Date.parse that adds support for parsing ES5 simplified ISO 8601 strings to all browsers. *This
+is the non-conformant edition of Universal JavaScript Date.parse for ISO 8601*. It violates the ES5 errata spec in the
+following helpful ways:
 
+1. Hyphens and colons are optional.
+2. Dates and times without an explicit timezone are treated as being in local time.
+3. Time part can be separated by a space in addition to "T". (This is a violation of ISO 8601:2004(E).)
+4. Time part separator is optional.
+5. Sub-second precision part can also be separated by comma.
+6. Sub-second precision part can be any arbitrary precision, milliseconds or smaller.
+
+If you want an ES5-conformant shim, please use the [master branch](https://github.com/csnover/js-iso8601/tree/master)
+instead.
 
 Caveats
 -------
 
-1. This library strictly implements the simplified ISO 8601 date time string format specified in the
-   [ES5 Errata](http://wiki.ecmascript.org/doku.php?id=es3.1:es3.1_proposal_working_draft) (§15.9.1.15) and will *not*
-   parse ISO 8601 variants that would otherwise be considered valid under ISO 8601:2004(E).
+1. This library does *not* strictly implement the simplified ISO 8601 date time string format specified in the
+   [ES5 Errata](http://wiki.ecmascript.org/doku.php?id=es3.1:es3.1_proposal_working_draft) and will not work
+   identically to conforming implementations.
 2. Creating a new date using the `new Date(dateString)` form will not be fixed by this shim.
-3. ES5 §15.9.4.2 states that parsing for Date Time String Format must occur prior to any implementation-specific date
-   format parsing; therefore, this JavaScript implementation will always be used in lieu of native browser support
-   for parsing the simplified ISO 8601 date format.
+3. Due to its non-standard behaviour, this JavaScript implementation will always be used in lieu of native browser
+   support for parsing the simplified ISO 8601 date format.
 
 Unit tests
 ----------
--- tests/test.js
+++ tests/test.js
@@ -2,20 +2,22 @@ var sixHours       = 6 * 60 * 60 * 1000,
     sixHoursThirty = sixHours + 30 * 60 * 1000;
 
 test('date-part', 16, function () {
-    strictEqual(Date.parse('1970-01-01'), Date.UTC(1970, 0, 1, 0, 0, 0, 0), 'Unix epoch');
+    strictEqual(Date.parse('1970-01-01'), +new Date(1970, 0, 1, 0, 0, 0, 0), 'Unix epoch');
 
-    strictEqual(Date.parse('2001'),       Date.UTC(2001, 0, 1, 0, 0, 0, 0), '2001');
-    strictEqual(Date.parse('2001-02'),    Date.UTC(2001, 1, 1, 0, 0, 0, 0), '2001-02');
-    strictEqual(Date.parse('2001-02-03'), Date.UTC(2001, 1, 3, 0, 0, 0, 0), '2001-02-03');
+    strictEqual(Date.parse('2001'),       +new Date(2001, 0, 1, 0, 0, 0, 0), '2001');
+    strictEqual(Date.parse('2001-02'),    +new Date(2001, 1, 1, 0, 0, 0, 0), '2001-02');
+    strictEqual(Date.parse('2001-02-03'), +new Date(2001, 1, 3, 0, 0, 0, 0), '2001-02-03');
 
-    strictEqual(Date.parse('-002001'),       Date.UTC(-2001, 0, 1, 0, 0, 0, 0), '-002001');
-    strictEqual(Date.parse('-002001-02'),    Date.UTC(-2001, 1, 1, 0, 0, 0, 0), '-002001-02');
-    strictEqual(Date.parse('-002001-02-03'), Date.UTC(-2001, 1, 3, 0, 0, 0, 0), '-002001-02-03');
+    strictEqual(Date.parse('-002001'),       +new Date(-2001, 0, 1, 0, 0, 0, 0), '-002001');
+    strictEqual(Date.parse('-002001-02'),    +new Date(-2001, 1, 1, 0, 0, 0, 0), '-002001-02');
+    strictEqual(Date.parse('-002001-02-03'), +new Date(-2001, 1, 3, 0, 0, 0, 0), '-002001-02-03');
 
-    strictEqual(Date.parse('+010000-02'),    Date.UTC(10000, 1, 1, 0, 0, 0, 0), '+010000-02');
-    strictEqual(Date.parse('+010000-02-03'), Date.UTC(10000, 1, 3, 0, 0, 0, 0), '+010000-02-03');
-    strictEqual(Date.parse('-010000-02'),    Date.UTC(-10000, 1, 1, 0, 0, 0, 0), '-010000-02');
-    strictEqual(Date.parse('-010000-02-03'), Date.UTC(-10000, 1, 3, 0, 0, 0, 0), '-010000-02-03');
+    strictEqual(Date.parse('+010000-02'),    +new Date(10000, 1, 1, 0, 0, 0, 0), '+010000-02');
+    strictEqual(Date.parse('+010000-02-03'), +new Date(10000, 1, 3, 0, 0, 0, 0), '+010000-02-03');
+    strictEqual(Date.parse('-010000-02'),    +new Date(-10000, 1, 1, 0, 0, 0, 0), '-010000-02');
+    strictEqual(Date.parse('-010000-02-03'), +new Date(-10000, 1, 3, 0, 0, 0, 0), '-010000-02-03');
+
+    strictEqual(Date.parse('19700101'),      +new Date(1970, 0, 1, 0, 0, 0, 0), 'non-hyphenated Unix epoch');
 
     // 2 digit years incorrectly parsed as though they were prefixed with 19
     /* disabled, since no native implementations get this right either
@@ -26,16 +28,15 @@ test('date-part', 16, function () {
     ok(isNaN(Date.parse('asdf')), 'invalid YYYY (non-digits)');
     ok(isNaN(Date.parse('1970-as-df')), 'invalid YYYY-MM-DD (non-digits)');
     ok(isNaN(Date.parse('1970-01-')), 'invalid YYYY-MM- (extra hyphen)');
-    ok(isNaN(Date.parse('19700101')), 'invalid YYYY-MM-DD (missing hyphens)');
-    ok(isNaN(Date.parse('197001')), 'ambiguous YYYY-MM/YYYYYY (missing plus/minus or hyphen)');
+    strictEqual(Date.parse('197001'), +new Date(1970, 0, 1, 0, 0, 0, 0), 'non-hyphenated year-month');
 
     // TODO: Test for invalid YYYYMM and invalid YYYYY?
 });
 
 test('date-time', 31, function () {
-    strictEqual(Date.parse('2001-02-03T04:05'),        Date.UTC(2001, 1, 3, 4, 5, 0, 0), '2001-02-03T04:05');
-    strictEqual(Date.parse('2001-02-03T04:05:06'),     Date.UTC(2001, 1, 3, 4, 5, 6, 0), '2001-02-03T04:05:06');
-    strictEqual(Date.parse('2001-02-03T04:05:06.007'), Date.UTC(2001, 1, 3, 4, 5, 6, 7), '2001-02-03T04:05:06.007');
+    strictEqual(Date.parse('2001-02-03T04:05'),        +new Date(2001, 1, 3, 4, 5, 0, 0), '2001-02-03T04:05');
+    strictEqual(Date.parse('2001-02-03T04:05:06'),     +new Date(2001, 1, 3, 4, 5, 6, 0), '2001-02-03T04:05:06');
+    strictEqual(Date.parse('2001-02-03T04:05:06.007'), +new Date(2001, 1, 3, 4, 5, 6, 7), '2001-02-03T04:05:06.007');
 
     strictEqual(Date.parse('2001-02-03T04:05Z'),        Date.UTC(2001, 1, 3, 4, 5, 0, 0), '2001-02-03T04:05Z');
     strictEqual(Date.parse('2001-02-03T04:05:06Z'),     Date.UTC(2001, 1, 3, 4, 5, 6, 0), '2001-02-03T04:05:06Z');
@@ -57,20 +58,20 @@ test('date-time', 31, function () {
     strictEqual(Date.parse('2001-02-03T04:05:06+06:30'),     Date.UTC(2001, 1, 3, 4, 5, 6, 0) - sixHoursThirty, '2001-02-03T04:05:06+06:30');
     strictEqual(Date.parse('2001-02-03T04:05:06.007+06:30'), Date.UTC(2001, 1, 3, 4, 5, 6, 7) - sixHoursThirty, '2001-02-03T04:05:06.007+06:30');
 
-    strictEqual(Date.parse('2001T04:05:06.007'),             Date.UTC(2001, 0, 1, 4, 5, 6, 7), '2001T04:05:06.007');
-    strictEqual(Date.parse('2001-02T04:05:06.007'),          Date.UTC(2001, 1, 1, 4, 5, 6, 7), '2001-02T04:05:06.007');
-    strictEqual(Date.parse('2001-02-03T04:05:06.007'),       Date.UTC(2001, 1, 3, 4, 5, 6, 7), '2001-02-03T04:05:06.007');
+    strictEqual(Date.parse('2001T04:05:06.007'),             +new Date(2001, 0, 1, 4, 5, 6, 7), '2001T04:05:06.007');
+    strictEqual(Date.parse('2001-02T04:05:06.007'),          +new Date(2001, 1, 1, 4, 5, 6, 7), '2001-02T04:05:06.007');
+    strictEqual(Date.parse('2001-02-03T04:05:06.007'),       +new Date(2001, 1, 3, 4, 5, 6, 7), '2001-02-03T04:05:06.007');
     strictEqual(Date.parse('2001-02-03T04:05:06.007-06:30'), Date.UTC(2001, 1, 3, 4, 5, 6, 7) + sixHoursThirty, '2001-02-03T04:05:06.007-06:30');
 
-    strictEqual(Date.parse('-010000T04:05'),       Date.UTC(-10000, 0, 1, 4, 5, 0, 0), '-010000T04:05');
-    strictEqual(Date.parse('-010000-02T04:05'),    Date.UTC(-10000, 1, 1, 4, 5, 0, 0), '-010000-02T04:05');
-    strictEqual(Date.parse('-010000-02-03T04:05'), Date.UTC(-10000, 1, 3, 4, 5, 0, 0), '-010000-02-03T04:05');
+    strictEqual(Date.parse('-010000T04:05'),       +new Date(-10000, 0, 1, 4, 5, 0, 0), '-010000T04:05');
+    strictEqual(Date.parse('-010000-02T04:05'),    +new Date(-10000, 1, 1, 4, 5, 0, 0), '-010000-02T04:05');
+    strictEqual(Date.parse('-010000-02-03T04:05'), +new Date(-10000, 1, 3, 4, 5, 0, 0), '-010000-02-03T04:05');
 
-    ok(isNaN(Date.parse('1970-01-01 00:00:00')), 'invalid date-time (missing T)');
-    ok(isNaN(Date.parse('1970-01-01T00:00:00.000000')), 'invalid date-time (too many characters in millisecond part)');
-    ok(isNaN(Date.parse('1970-01-01T00:00:00,000')), 'invalid date-time (comma instead of dot)');
-    ok(isNaN(Date.parse('1970-01-01T00:00:00+0630')), 'invalid date-time (missing colon in timezone part)');
-    ok(isNaN(Date.parse('1970-01-01T0000')), 'invalid date-time (missing colon in time part)');
+    strictEqual(Date.parse('1970-01-01 00:00:00Z'),       Date.UTC(1970, 0, 1, 0, 0, 0, 0), 'space-separated datetime');
+    strictEqual(Date.parse('1970-01-01T00:00:00.987654'), +new Date(1970, 0, 1, 0, 0, 0, 987), 'extended sub-second precision');
+    strictEqual(Date.parse('1970-01-01T00:00:00,123'),    +new Date(1970, 0, 1, 0, 0, 0, 123), 'comma-delimited milliseconds');
+    strictEqual(Date.parse('1970-01-01T00:00:00+0630'),   Date.UTC(1970, 0, 1, 0, 0, 0, 0) - sixHoursThirty, 'colon-free timezone part');
+    strictEqual(Date.parse('1970-01-01T0000'),            +new Date(1970, 0, 1, 0, 0, 0, 0), 'colon-free time part');
     ok(isNaN(Date.parse('1970-01-01T00:00.000')), 'invalid date-time (msec with missing seconds)');
 
     // TODO: DRY
